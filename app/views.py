a667d5dd07a952e39628ad6be91e68228fd5a6cf
Kuba Pawlaczyk <kuba@crunchbang>
2013-11-18 12:27:19 +0100

    Added base html template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
+from flask import render_template
+from models import Post
 from app import app
 
 @app.route('/')
 def index():
-    return 'root page'
+    return render_template('starter-template.html')
+
+@app.route('/posts/')
+def posts():
+    posts = Post.query.all()
+    return render_template('posts.html', title = 'Posts', posts = posts)
