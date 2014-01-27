d0d2eeb96f56c5a7cb690d47ae2cc6ada9abbe1e
Kuba Pawlaczyk <pawlaczyk.jakub@gmail.com>
2014-01-27 22:50:23 +0100

    Added docs

--- manager.py
+++ manager.py
@@ -1,4 +1,23 @@
 #!flask/bin/python
+""" This module imports Flask-Manager script, adds our create_db command
+and run it. You can pass following arguments:
+    * create_db => creates sqlite database and all the tables
+    * shell => runs python shell inside application context
+    * runserver => runs Flask development server
+    * db => performs database migrations
+        * db init => generate new migration
+        * db migrate => generate automatic revision
+        * db current => display current revision
+        * db upgrade => upgrade to later version
+        * db downgrade => revert to previous version
+        * db history => list changes
+        * db revision => create new revision file
+        * db stamp => 'stamp' the revision table with giver revision
+
+    optional arguments:
+        -h, --help shows help message 
+"""
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager, Command
@@ -7,8 +26,10 @@ from app import app, db, migrate, models
 
 
 class CreateDb(Command):
+    """This class inherit from Flask-manager to add create_db command"""
 
     def run(self):
+        """ Create database with all tables and print log to std.out"""
         print 'Creating the database.'
         db.create_all()
 
