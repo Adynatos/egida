495812abbf35382081c69edf225e08ecafb64872
Kuba Pawlaczyk <pawlaczyk.jakub@gmail.com>
2014-01-28 01:45:58 +0100

    Generated documentation

--- app/views.py
+++ app/views.py
@@ -47,7 +47,8 @@ def index():
 @app.route('/new_post', methods=['GET', 'POST'])
 @login_required
 def new_post():
-    """Create and handle form for  adding posts,
+    """Create and handle form for adding posts.
+    If form validate adds data to database.
     Renders template for adding new post.
     Accept both POST and GET request"""
 
@@ -71,6 +72,9 @@ def new_post():
 @app.route('/posts/')
 #@login_required
 def posts():
+    """ Query the database for post, ordered descending by publication
+    date, then render template to display them
+    """
     posts = Post.query.order_by(Post.pub_date.desc())
     return render_template('posts.html', posts=posts, user=g.user)
 
@@ -78,6 +82,11 @@ def posts():
 @app.route('/posts/<post_id>', methods=['GET', 'POST'])
 @login_required
 def view_post(post_id):
+    """Displays post page for post with given id or 404 error if post with
+    that id doesn't exist.
+    Also handle adding comments (generate form, validate data and update
+    database)
+    """
     post = Post.query.filter_by(id=post_id).first_or_404()
     comments = Comment.query.filter_by(post_id=post_id).order_by(Comment.pub_date.asc())
 
@@ -97,6 +106,9 @@ def view_post(post_id):
 @app.route('/tags/<tag_name>')
 @login_required
 def posts_with_tag(tag_name):
+    """ Displays all posts with given tag, or 404 error if tag doesn't
+    exist
+    """
     tag = Tag.query.filter_by(name=tag_name).first_or_404()
     posts = tag.posts.all()
     return render_template('posts.html', posts=posts, user=g.user)
@@ -105,6 +117,8 @@ def posts_with_tag(tag_name):
 @app.route('/search', methods=['POST'])
 @login_required
 def search():
+    """ Handler for search request. Redirects to page with search results
+    """
     if not g.searching_form.validate_on_submit():
         return redirect(url_for('index'))
     return redirect(url_for('search_results', query=g.searching_form.search.data))
@@ -113,6 +127,9 @@ def search():
 @app.route('/search_results/<query>')
 @login_required
 def search_results(query):
+    """ Displays search results for the given query.
+    Search all posts' body and display up to MAX_SEARCH_RESULTS results
+    """
     results = Post.query.whoosh_search(query, MAX_SEARCH_RESULTS).all()
     return render_template('search_results.html', query=query, results=results)
 
@@ -120,6 +137,9 @@ def search_results(query):
 @app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
 def login():
+    """ Login function create and validate LoginForm.
+    Uses openid for authentication. 
+    """
     if g.user is not None and g.user.is_authenticated():
         return redirect(url_for('index'))
     form = LoginForm()
@@ -132,12 +152,19 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """ Deauthenticate current user
+    """
     logout_user()
     return redirect(url_for('index'))
 
 
 @oid.after_login
 def after_login(resp):
+    """ Function runs after succesful login
+    If user logs in for the first time he is added to the database.
+    If remember_me field is set true then it sets proper cookies for user's
+    session
+    """
     if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
